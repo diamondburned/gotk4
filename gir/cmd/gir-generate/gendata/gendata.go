diamondburned/gotk4

commit f039f7d27450f9c31c62358c2561653d6a0207a2

Add genmain.Data.KnownPackages
With just Packages, generating gotk4-dependent external libraries was
basically impossible. This commit fixes that.

gotk4-adwaita relies on this change.

gir/cmd/gir-generate/genmain/genmain.go
@@ -73,6 +73,9 @@ type Data struct {
 	// generated. If the list of namespaces is nil, then everything is
 	// generated.
 	Packages []Package
+	// KnownPackages is similar to Packages, but no packages in this list will
+	// be used to generate code. This list automatically includes Packages.
+	KnownPackages []Package
 	// ImportOverrides is the list of imports to defer to another library,
 	// usually because it's tedious or impossible to generate.
 	//
@@ -117,6 +120,7 @@ func Run(data Data) {
 	ParseFlag()
 
 	repos := MustLoadPackages(data.Packages)
+	MustAddPackages(&repos, data.KnownPackages)
 	PrintAddedPkgs(repos)
 
 	if ListPkg {
@@ -154,8 +158,9 @@ func Generate(repos gir.Repositories, data Data) {
 		log.Fatalln("failed to clean output directory:", err)
 	}
 
-	if errors := GenerateAll(gen, Output, data.GenerateExceptions); len(errors) > 0 {
-		for _, err := range errors {
+	genErrs := GeneratePackages(gen, Output, data.Packages, data.GenerateExceptions)
+	if len(genErrs) > 0 {
+		for _, err := range genErrs {
 			log.Println("generation error:", err)
 		}
 		os.Exit(1)

gir/cmd/gir-generate/genmain/genutil.go
@@ -212,11 +212,13 @@ func GenerateAll(gen *girgen.Generator, dst string, except []string) []error {
 // GeneratePackages generates the given pkgs list into the given dst directory.
 // It uses WriteNamespace to do so. The namespaces will be generated in
 // parallel. Most external GIR generators should call this.
-func GeneratePackages(gen *girgen.Generator, dst string, pkgs []Package) []error {
+func GeneratePackages(gen *girgen.Generator, dst string, pkgs []Package, except []string) []error {
 	sema := semaphore.NewWeighted(int64(runtime.GOMAXPROCS(-1)))
 
 	var wg sync.WaitGroup
 	defer wg.Wait()
+
+	exemptions := StringSet(except)
 
 	var errMut sync.Mutex
 	var errors []error
@@ -250,6 +252,11 @@ func GeneratePackages(gen *girgen.Generator, dst string, pkgs []Package) []error
 				namespace := repos.FindNamespace(wantedName)
 				if namespace == nil {
 					return []error{fmt.Errorf("namespace %q not found", wantedName)}
+				}
+
+				_, exempted := exemptions[gir.VersionedNamespace(namespace.Namespace)]
+				if exempted {
+					continue
 				}
 
 				genNamespace(namespace.Namespace)

commit 11b1a9e5d327ff105d507bb997840758871673a5

Fix missing packages regression caused by f039f7d
GeneratePackages was broken. This commit fixes it, causing missing files
from previous commits to be generated again.

gir/cmd/gir-generate/gendata/gendata.go
@@ -17,6 +17,25 @@ import (
 
 const Module = "github.com/diamondburned/gotk4/pkg"
 
+// Main contains all of gendata's variables as one big genmain.Data value.
+//
+// This stays ugly just because it's the main gotk4 package with exposed
+// gendata. Don't actually do this; just make a global genmain.Data instead.
+var Main = genmain.Data{
+	Module:                Module,
+	Packages:              Packages,
+	ImportOverrides:       ImportOverrides,
+	PkgExceptions:         PkgExceptions,
+	GenerateExceptions:    GenerateExceptions,
+	PkgGenerated:          PkgGenerated,
+	Preprocessors:         Preprocessors,
+	Postprocessors:        Postprocessors,
+	ExtraGoContents:       ExtraGoContents,
+	Filters:               Filters,
+	ProcessConverters:     ConversionProcessors,
+	DynamicLinkNamespaces: DynamicLinkNamespaces,
+}
+
 type Package struct {
 	PkgName    string   // pkg-config name
 	Namespaces []string // refer to ./cmd/gir_namespaces

gir/cmd/gir-generate/genmain/genmain.go
@@ -115,6 +115,28 @@ type Data struct {
 	DynamicLinkNamespaces []string
 }
 
+// Overlay joins the given list of data into a single Data. The last Data in the
+// list will be used for generation.
+func Overlay(data ...Data) Data {
+	overlay := data[:len(data)-1]
+	last := data[len(data)-1]
+
+	for _, datum := range overlay {
+		if last.ExternOverrides == nil {
+			last.ExternOverrides = make(map[string]gir.Repositories)
+		}
+
+		last.KnownPackages = append(last.KnownPackages, datum.Packages...)
+		last.ExternOverrides[datum.Module] = MustLoadPackages(datum.Packages)
+		last.Preprocessors = append(last.Preprocessors, datum.Preprocessors...)
+		last.Filters = append(last.Filters, datum.Filters...)
+		last.ProcessConverters = append(last.ProcessConverters, datum.ProcessConverters...)
+		last.DynamicLinkNamespaces = append(last.DynamicLinkNamespaces, datum.DynamicLinkNamespaces...)
+	}
+
+	return last
+}
+
 // Run runs the application.
 func Run(data Data) {
 	ParseFlag()

gir/cmd/gir-generate/genmain/genutil.go
@@ -223,7 +223,12 @@ func GeneratePackages(gen *girgen.Generator, dst string, pkgs []Package, except 
 	var errMut sync.Mutex
 	var errors []error
 
-	genNamespace := func(namespace *gir.Namespace) {
+	genNamespace := func(namespace gir.Namespace) {
+		_, exempted := exemptions[gir.VersionedNamespace(&namespace)]
+		if exempted {
+			return
+		}
+
 		ng := gen.UseNamespace(namespace.Name, namespace.Version)
 		if ng == nil {
 			log.Fatalln("cannot find namespace", namespace.Name, "v"+namespace.Version)
@@ -247,34 +252,33 @@ func GeneratePackages(gen *girgen.Generator, dst string, pkgs []Package, except 
 	repos := gen.Repositories()
 
 	for _, pkg := range pkgs {
-		if pkg.Namespaces != nil {
-			for _, wantedName := range pkg.Namespaces {
-				namespace := repos.FindNamespace(wantedName)
-				if namespace == nil {
-					return []error{fmt.Errorf("namespace %q not found", wantedName)}
-				}
-
-				_, exempted := exemptions[gir.VersionedNamespace(namespace.Namespace)]
-				if exempted {
-					continue
-				}
-
-				genNamespace(namespace.Namespace)
+		if pkg.Namespaces == nil {
+			repos := repos.FromPkg(pkg.Name)
+			if repos == nil {
+				return []error{fmt.Errorf("package %q not found", pkg.Name)}
 			}
+
+			for _, repo := range repos {
+				for _, namespace := range repo.Namespaces {
+					genNamespace(namespace)
+				}
+			}
+
+			continue
 		}
 
-		repo := repos.FromPkg(pkg.Name)
-		if repo == nil {
-			return []error{fmt.Errorf("package %q not found", pkg.Name)}
-		}
+		for _, wantedName := range pkg.Namespaces {
+			namespace := repos.FindNamespace(wantedName)
+			if namespace == nil {
+				return []error{fmt.Errorf("namespace %q not found", wantedName)}
+			}
 
-		for _, namespace := range repo.Namespaces {
-			genNamespace(&namespace)
+			log.Println("generating chosen namespace", namespace.Namespace.Name, "for", pkg.Name)
+			genNamespace(*namespace.Namespace)
 		}
 	}
 
 	wg.Wait()
-
 	return errors
 }
 

gir/cmd/gir-generate/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/diamondburned/gotk4/gir/cmd/gir-generate/gendata"
@@ -12,23 +13,9 @@ import (
 func main() {
 	if os.Getenv("GOTK4_RUNTIME_LINK") == "1" {
 		// TODO: remove this when RuntimeLinkMode is working.
+		log.Println("warning: GOTK4_RUNTIME_LINK is set to 1")
 		girgen.DefaultLinkMode = types.RuntimeLinkMode
 	}
 
-	// This stays ugly just because it's the main gotk4 package with exposed
-	// gendata. Don't actually do this; just make a global genmain.Data instead.
-	genmain.Run(genmain.Data{
-		Module:                gendata.Module,
-		Packages:              gendata.Packages,
-		ImportOverrides:       gendata.ImportOverrides,
-		PkgExceptions:         gendata.PkgExceptions,
-		GenerateExceptions:    gendata.GenerateExceptions,
-		PkgGenerated:          gendata.PkgGenerated,
-		Preprocessors:         gendata.Preprocessors,
-		Postprocessors:        gendata.Postprocessors,
-		ExtraGoContents:       gendata.ExtraGoContents,
-		Filters:               gendata.Filters,
-		ProcessConverters:     gendata.ConversionProcessors,
-		DynamicLinkNamespaces: gendata.DynamicLinkNamespaces,
-	})
+	genmain.Run(gendata.Main)
 }

gir/gir.go
@@ -221,13 +221,14 @@ func (repos Repositories) FromGIRFile(girFile string) *PkgRepository {
 }
 
 // FromPkg finds the repository from the given package name.
-func (repos Repositories) FromPkg(pkg string) *PkgRepository {
+func (repos Repositories) FromPkg(pkg string) []PkgRepository {
+	var found []PkgRepository
 	for i, repo := range repos {
 		if repo.Pkg == pkg {
-			return &repos[i]
+			found = append(found, repos[i])
 		}
 	}
-	return nil
+	return found
 }
 
 // NamespaceFindResult is the result returned from FindNamespace.
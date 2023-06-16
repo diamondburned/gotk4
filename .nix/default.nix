179cbe3099485561eb5bbce55f0d377aa1c5e6e4
Luke Shumaker <lukeshu@lukeshu.com>
2023-06-16 13:28:08 -0600

    .nix: Inline src.nix into default.nix

--- .nix/default.nix
+++ .nix/default.nix
@@ -1,13 +1,23 @@
 { action }:
 
-let src = import ./src.nix;
+let
 
-in import "${src.gotk4-nix}/${action}.nix" {
+	systemPkgs = import <nixpkgs> {};
+
+	# gotk4-nix = ../../gotk4-nix;
+	gotk4-nix = systemPkgs.fetchFromGitHub {
+		owner = "diamondburned";
+		repo  = "gotk4-nix";
+		rev   = "0a50408da4eb59ad4500db49785676714f4bd4e6";
+		hash  = "sha256:1ryxxkxly298yr3m0868g69jgk8gagvg6zmpknv2l7jd8x3w4pz7";
+	};
+
+in import "${gotk4-nix}/${action}.nix" {
 	base = {
 		pname   = "gotk4";
 		version = "dev";
 	};
-	pkgs = import "${src.gotk4-nix}/pkgs.nix" {
+	pkgs = import "${gotk4-nix}/pkgs.nix" {
 		sourceNixpkgs = builtins.fetchTarball {
 			name   = "gotk4-nixpkgs";
 			url    = "https://github.com/NixOS/nixpkgs/archive/fbfb79400a08bf754e32b4d4fc3f7d8f8055cf94.tar.gz";

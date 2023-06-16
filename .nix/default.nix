54f737a86e83345577f6ce66da830570d4daf274
Luke Shumaker <lukeshu@lukeshu.com>
2023-06-16 13:37:45 -0600

    docs: Add comments to default.nix

--- .nix/default.nix
+++ .nix/default.nix
@@ -4,10 +4,14 @@ let
 
 	systemPkgs = import <nixpkgs> {};
 
+	# gotk4-nix pins the version of
+	# <https://github.com/diamondburned/gotk4-nix> to use.
+	#
 	# gotk4-nix = ../../gotk4-nix;
 	gotk4-nix = systemPkgs.fetchFromGitHub {
 		owner = "diamondburned";
 		repo  = "gotk4-nix";
+		# This is a commit from 2022-05-29.
 		rev   = "0a50408da4eb59ad4500db49785676714f4bd4e6";
 		hash  = "sha256:1ryxxkxly298yr3m0868g69jgk8gagvg6zmpknv2l7jd8x3w4pz7";
 	};
@@ -18,11 +22,17 @@ in import "${gotk4-nix}/${action}.nix" {
 		version = "dev";
 	};
 	pkgs = import "${gotk4-nix}/pkgs.nix" {
+		# sourceNixpkgs overrides the nixpkgs version pinned in
+		# <https://github.com/diamondburned/gotk4-nix/blob/main/src.nix>.
 		sourceNixpkgs = builtins.fetchTarball {
 			name   = "gotk4-nixpkgs";
+			# This is pinning an older (2021-06-06) revision of nixpkgs than
+			# the pinned gotk4-nix version pins (2022-01-29).
 			url    = "https://github.com/NixOS/nixpkgs/archive/fbfb79400a08bf754e32b4d4fc3f7d8f8055cf94.tar.gz";
 			sha256 = "0pgyx1l1gj33g5i9kwjar7dc3sal2g14mhfljcajj8bqzzrbc3za";
 		};
+		# useFetched specifies whether to use the system copy of nixpkgs
+		# (false), or whether to force use of the pinned version (true).
 		useFetched = true;
 	};
 }
